be3d0f7040568a952f11f77f68bf55682fa1e36e
WINDOWS <manoranjith5453@gmail.com>
2025-02-17 12:28:06 +0530

    signin update

--- app/src/main/java/com/example/hashtag/GetstartedActivity.java
+++ app/src/main/java/com/example/hashtag/GetstartedActivity.java
@@ -1,6 +1,10 @@
 package com.example.hashtag;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class GetstartedActivity extends AppCompatActivity {
+
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,16 @@ public class GetstartedActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        but=findViewById(R.id.getstartedbutton);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent it=new Intent(GetstartedActivity.this, MainActivity.class);
+                startActivity(it);
+            }
         });
     }
 }
